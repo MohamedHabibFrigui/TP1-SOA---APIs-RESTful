aae4a1a6de7d8b31bd3601fb78e94b1018dcec11
Mohamed Habib Frigui <mohamedhabibfrigui9@gmail.com>
2025-02-01 09:55:24 +0100

    Test OpenWeatherMap API with "axios"

---
+++ test_axios.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+
+const API_KEY = "38f9264b8e345e5059d64b5e08c19663";
+const BASE_URL =
+  "http://api.openweathermap.org/data/2.5/weather?appid=" + API_KEY + "&q=";
+
+async function getWeatherData(city) {
+  const url = BASE_URL + city + "&units=metric&lang=fr";
+  try {
+    const response = await axios.get(url);
+    const weatherData = response.data;
+    console.log("Weather Description:", weatherData.weather[0].description);
+    console.log("Temperature:", weatherData.main.temp + "°C");
+    console.log("Humidity:", weatherData.main.humidity + "%");
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+  }
+}
+
+getWeatherData("Sousse");
